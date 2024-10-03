0db7cd6d11e313892a8a30e66d0944788fd14cd2
rohitupadhhyay <upadhyay8486@gmail.com>
2024-10-03 19:23:20 +0530

    commit

--- crypto-hash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-
-const cryptohash = (...inputs) => {
-  const hash = crypto.createHash("SHA256");
-  hash.update(inputs.sort().join(""));
-  return hash.digest("hex");
-};
-
-module.exports = cryptohash;
